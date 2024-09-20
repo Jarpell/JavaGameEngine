97f18aa3780a85b822dea818fc8aa10234512367
Jared <jaredwpelletier@gmail.com>
2024-09-19 20:47:37 -0500

    Initial Commit

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,133 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import entites.Camera;
+import entites.Entity;
+import models.RawModel;
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+
+public class MainGameLoop {
+	/**
+	 * Creates a display and then continuously updates the display until the user tries to close it. 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
+		
+		float[] vertices = {			
+				-0.5f,0.5f,0,	
+				-0.5f,-0.5f,0,	
+				0.5f,-0.5f,0,	
+				0.5f,0.5f,0,		
+				
+				-0.5f,0.5f,1,	
+				-0.5f,-0.5f,1,	
+				0.5f,-0.5f,1,	
+				0.5f,0.5f,1,
+				
+				0.5f,0.5f,0,	
+				0.5f,-0.5f,0,	
+				0.5f,-0.5f,1,	
+				0.5f,0.5f,1,
+				
+				-0.5f,0.5f,0,	
+				-0.5f,-0.5f,0,	
+				-0.5f,-0.5f,1,	
+				-0.5f,0.5f,1,
+				
+				-0.5f,0.5f,1,
+				-0.5f,0.5f,0,
+				0.5f,0.5f,0,
+				0.5f,0.5f,1,
+				
+				-0.5f,-0.5f,1,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1
+				
+		};
+		
+		float[] textureCoords = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+
+		};
+
+		 RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
+		 
+		 TexturedModel texturedModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("mid")));
+		 
+		 Entity entity = new Entity(texturedModel, new Vector3f(0,0,-5),0,0,0,1);
+		 
+		 Camera camera = new Camera();
+		 
+		while (!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 1);
+			camera.move();
+			renderer.prepare();
+			shader.start();
+			shader.loadViewMatrix(camera);
+			renderer.render(entity,shader);
+			shader.stop();
+			DisplayManager.updateDisplay();
+		}
+		shader.cleaUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+	}
+
+}
